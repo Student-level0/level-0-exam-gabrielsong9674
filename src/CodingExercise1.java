99d2d727ee6639b72158bbe098066674c2c7c650
gabrielsong9674 <league@worthymini-5.attlocal.net>
2018-01-07 17:12:52 -0800

    level0exam

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -6,14 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class CodingExercise1 {
 
     public static void main(String[] args) {
+    	 Robot r = new Robot();
    	 // 3. ask the user what color they would like the Robot to draw
-
+String color = JOptionPane.showInputDialog("What color would you like the Robot to draw?");
    	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
-
+if(color.equals("red")) {
+	r.setPenColor(250, 0, 0);
+}
+else {
+	r.setPenColor(0, 0, 250);
+}
    	 // 2. set the pen width to 10
-
+r.setPenWidth(10);
+r.penDown();
    	 // 1. make the Robot draw a shape
-   	 
+  
+   	 for(int i = 0; i <3; i++) {
+   		r.setSpeed(10);
+   		r.move(100);
+   		r.turn(120);
+   	 }
     }
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -10,7 +10,13 @@ import javax.swing.JOptionPane;
  **/
 
 public class CodingExercise2 {
-
+public static void main(String[] args) {
+	String response = JOptionPane.showInputDialog("How old are you?");
+	int age = Integer.parseInt(response);
+	if(age > 30) {
+		JOptionPane.showMessageDialog(null, "You are too old to play this game.");
+	}
+}
 }
 
 
